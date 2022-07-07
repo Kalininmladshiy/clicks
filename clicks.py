361d093a3bb26cb6870bf5c8e8ba9c2edfffaeb7
Kalininmladshiy <106922768+Kalininmladshiy@users.noreply.github.com>
2022-07-07 16:11:49 +0300

    Changed variable's name

--- clicks.py
+++ clicks.py
@@ -4,31 +4,31 @@ from urllib.parse import urlparse
 
 
 def shorten_link(token, url):
-    url_bitly = 'https://api-ssl.bitly.com/v4/shorten'
+    bitly_url = 'https://api-ssl.bitly.com/v4/shorten'
     headers = {'Authorization': 'Bearer {}'.format(token)}
     body = {"long_url": url}
-    response = requests.post(url_bitly, json=body, headers=headers)
+    response = requests.post(bitly_url, json=body, headers=headers)
     response.raise_for_status()
     bitlink = response.json()['id']
     return bitlink
 
 
 def count_clicks(token, bitlink):
-    url_bitly = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(bitlink)
+    bitly_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(bitlink)
     headers = {'Authorization': 'Bearer {}'.format(token)}
     payload = {
         'units': -1,
         'unit': 'day',
     }
-    response = requests.get(url_bitly, params=payload, headers=headers)
+    response = requests.get(bitly_url, params=payload, headers=headers)
     response.raise_for_status()
     return response.json()['total_clicks']
 
 
 def is_bitlink(url):
-    url_bitly = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(url)
+    bitly_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(url)
     headers = {'Authorization': 'Bearer {}'.format(token)}
-    response = requests.get(url_bitly, headers=headers)
+    response = requests.get(bitly_url, headers=headers)
     return response.ok
 
 
